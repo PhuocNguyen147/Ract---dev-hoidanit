cccb95bf18c46bc1f5faee6563b2f2eddc772a35
Phuoc Nguyen <nguyen.huuphuoc147@gmail.com>
2024-10-05 08:03:59 +0700

    #31

--- src/component/learn/Mycomponent.jsx
+++ src/component/learn/Mycomponent.jsx
@@ -1,13 +1,26 @@
 import './Mycomponent.css'
 const MyComponent = () => {
+    const Agephuoc = 24
+    const Infor = {
+        name: "Phuoc",
+        age: 24
+    }
     return (
-        <div className='test'> Phuoc nguyen</div>
+        <>
+            <div className='test'> Phuoc nguyen {Agephuoc}</div>
+            <div> {JSON.stringify(Infor)}</div>
+        </>
+
     )
     console.log('my function');
 }
 const TestComponent = () => {
     return (
-        <div style={{ borderRadius: "10px" }}>
+        <div style={
+
+            { borderRadius: "10px" }
+
+        }>
             test
         </div>
     )
